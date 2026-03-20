e7ac7b878931c4c43e4026a5ea490253eb0612cf
FabianaValiente <121946293+FabianaValiente@users.noreply.github.com>
2026-03-20 11:04:49 -0300

    initial commit

---
+++ src/main/java/com/trainer/system/TrainerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.trainer.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainer/system/TrainerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainer.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
