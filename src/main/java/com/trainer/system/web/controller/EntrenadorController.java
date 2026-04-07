700e72cc28ebb95ce177a49745da472e44263b0f
FabianaValiente <121946293+FabianaValiente@users.noreply.github.com>
2026-04-07 16:57:16 -0300

    cambios menores

--- src/main/java/com/trainer/system/web/controller/EntrenadorController.java
+++ src/main/java/com/trainer/system/web/controller/EntrenadorController.java
@@ -5,11 +5,13 @@ import com.trainer.system.domain.service.EntrenadorService;
 import com.trainer.system.persistence.crud.CrudEntrenadorEntity;
 import com.trainer.system.persistence.entity.EntrenadorEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/entrenadores")
 public class EntrenadorController {
     private final EntrenadorService entrenadorService;
 
@@ -17,7 +19,7 @@ public class EntrenadorController {
         this.entrenadorService = entrenadorService;
     }
 
-    @GetMapping("/entrenadores")
+    @GetMapping
     public List<EntrenadorDto> getEntrenadores() {
         return (List<EntrenadorDto>) this.entrenadorService.getEntrenadores();
     }
